b539fe8c533d902cae5c1abe5aa3feb7e509eae3
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-26 23:50:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-
-import React from "react";
+import React, { createContext, useState } from "react";
 import './../styles/App.css';
-
+import Auth from "./Auth";
+export const AuthContext=createContext();
 const App = () => {
+  const [data, setdata] = useState(false);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <AuthContext.Provider value={{data,setdata}}>
+        <Auth />
+      </AuthContext.Provider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
